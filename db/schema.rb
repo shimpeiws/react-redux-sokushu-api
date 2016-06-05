2479483dee6fefb0a515e7b501071f2ad588b000
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-05 13:13:26 +0900

    Add schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160605015012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "content"
+    t.integer  "issue_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["issue_id"], name: "index_comments_on_issue_id", using: :btree
+  end
+
+  create_table "issues", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "comments", "issues"
 end
