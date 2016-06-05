aa78cf2bd8c7ff60deee946fe65d13b43ea17f78
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-05 15:21:34 +0900

    Add comments controller

---
+++ app/controllers/issue_comments_controller.rb
@@ -0,0 +1,42 @@
+class IssueCommentsController < ApplicationController
+  before_action :set_issue
+  before_action :set_comment, only: [:update, :destroy]
+
+  def create
+    @comment = Comment.new(issue_params)
+
+    unless @comment.save
+      return render json: @issue.errors, status: :unprocessable_entity
+    end
+
+    render json: @comment
+  end
+
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_name, :content)
+  end
+
+  def set_issue
+    @issue = Issue.find(params[:issue_id])
+    render nothing: true, status: :not_found unless @issue.present?
+  end
+
+  def set_comment
+    @comment = @issue.comment.find(comment_params[:id])
+    render nothing: true, status: :unprocessable_entity unless @comment.present?
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
-Rails.application.routes.draw do
-  resources :labels, defaults: {format: :json}
-  resources :issues, only: [:index, :show, :create, :update, :destroy], defaults: {format: :json}
+# == Route Map
+#
+#         Prefix Verb   URI Pattern                                      Controller#Action
+# issue_comments POST   /issues/:issue_id/comments(.:format)             issue_comments#create {:format=>:json}
+#  issue_comment PATCH  /issues/:issue_id/comments/:comment_id(.:format) issue_comments#update {:format=>:json}
+#                PUT    /issues/:issue_id/comments/:comment_id(.:format) issue_comments#update {:format=>:json}
+#                DELETE /issues/:issue_id/comments/:comment_id(.:format) issue_comments#destroy {:format=>:json}
+#         issues GET    /issues(.:format)                                issues#index {:format=>:json}
+#                POST   /issues(.:format)                                issues#create {:format=>:json}
+#          issue GET    /issues/:id(.:format)                            issues#show {:format=>:json}
+#                PATCH  /issues/:id(.:format)                            issues#update {:format=>:json}
+#                PUT    /issues/:id(.:format)                            issues#update {:format=>:json}
+#                DELETE /issues/:id(.:format)                            issues#destroy {:format=>:json}
+#         labels GET    /labels(.:format)                                labels#index {:format=>:json}
+#                POST   /labels(.:format)                                labels#create {:format=>:json}
+#          label GET    /labels/:id(.:format)                            labels#show {:format=>:json}
+#                PATCH  /labels/:id(.:format)                            labels#update {:format=>:json}
+#                PUT    /labels/:id(.:format)                            labels#update {:format=>:json}
+#                DELETE /labels/:id(.:format)                            labels#destroy {:format=>:json}
+#
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  resources :issues, only: [:index, :show, :create, :update, :destroy], defaults: {format: :json} do
+    resources :comments,controller: 'issue_comments', param: :comment_id, only: [:create, :update, :destroy]
+  end
+  resources :labels, defaults: {format: :json}
 end
