acb9cb83248831f242d31acfcabbb27c0b047ba4
Mamoru Amano <booktower103@gmail.com>
2016-06-08 22:53:55 +0900

    Add content to issue

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160605043320) do
+ActiveRecord::Schema.define(version: 20160608224900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160605043320) do
     t.integer  "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
   create_table "labels", force: :cascade do |t|
