8729aef76157abef400bcd6ae9f0e0731ee51c9f
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-08 21:51:51 +0900

    Allow CORS POST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,9 @@ class ApplicationController < ActionController::API
   before_filter :add_allow_credentials_headers
 
   def add_allow_credentials_headers
-    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-  end
-
-  def options
-    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   end
 end
