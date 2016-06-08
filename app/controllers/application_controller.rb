0e876008f55328b7adeafa2428085fc08072b0fc
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-08 21:48:47 +0900

    Fix CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  before_filter :add_allow_credentials_headers
+
+  def add_allow_credentials_headers
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+  end
+
+  def options
+    head :status => 200, :'Access-Control-Allow-Headers' => 'accept, content-type'
+  end
 end
