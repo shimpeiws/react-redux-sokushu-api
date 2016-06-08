eb22dc385aa8f702d974b0811dce96479f9fe6e9
Mamoru Amano <booktower103@gmail.com>
2016-06-08 23:40:53 +0900

    Annotate and validation

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,12 +4,15 @@
 #
 #  id         :integer          not null, primary key
 #  title      :string
-#  status     :integer
+#  status     :integer          default("open"), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  content    :text
 #
 
 class Issue < ApplicationRecord
+  validate :content, presence: true 
+
   has_many :comments, -> { order(created_at: :asc) }, class_name: 'Comment'
 
   enum status: { close: 0, open: 1 }
