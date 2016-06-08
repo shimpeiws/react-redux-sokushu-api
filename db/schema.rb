27003aab8651a06fb31a999d87757c2f9b774643
Mamoru Amano <booktower103@gmail.com>
2016-06-08 23:02:08 +0900

    Status default open

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160608224900) do
+ActiveRecord::Schema.define(version: 20160608224950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 20160608224900) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "title"
-    t.integer  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "status",     default: 1, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.text     "content"
   end
 
