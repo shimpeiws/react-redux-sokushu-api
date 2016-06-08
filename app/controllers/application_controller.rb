bdc1b08bd3254906610d2226096e82906ea9e75d
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-08 22:13:07 +0900

    Remove request forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery
-
   before_filter :allow_cross_domain_access
   def allow_cross_domain_access
     response.headers["Access-Control-Allow-Origin"] = "*"
