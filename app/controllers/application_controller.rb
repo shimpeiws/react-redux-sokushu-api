5a6fdb954a87c7e9feb29ef8146512ea8a4f152f
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-08 22:15:21 +0900

    Remove

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::API
-  before_filter :allow_cross_domain_access
-  def allow_cross_domain_access
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    response.headers["Access-Control-Allow-Methods"] = "PUT,DELETE,POST,GET,OPTIONS"
-  end
 end
