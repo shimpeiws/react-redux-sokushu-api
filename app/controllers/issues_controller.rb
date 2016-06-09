9399f462c578ac96ff6ed236e6e88eb07c8c8c77
Shimpei Takamatsu <shimpeiws@gmail.com>
2016-06-09 13:58:39 +0900

    Fix

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,7 +4,7 @@ class IssuesController < ApplicationController
   def index
     @issues = Issue.all.order('updated_at DESC')
 
-    @issues = issues.where(status: params[:status]) if params[:status].present?
+    @issues = @issues.where(status: params[:status]) if params[:status].present?
 
     render json: @issues
   end
